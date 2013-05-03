e4effd4e04f1170834c1de19cfa6093920e4e4ce
Máté Marjai <mate.marjai@sumup.com>
2013-05-03 16:10:55 +0100

    Correcting the merge conflict.

--- rebuild.sh
+++ rebuild.sh
@@ -2,8 +2,4 @@
 gem uninstall sumup-rack-rpc
 rm -rf ./sumup-rack-rpc-*.gem
 gem build sumup-rack-rpc.gemspec
-<<<<<<< HEAD
 gem install sumup-rack-rpc --local ./sumup-rack-rpc-*.gem --no-ri --no-rdoc
-=======
-gem install sumup-rack-rpc --local ./sumup-rack-rpc-*.gem --no-ri --no-rdoc
->>>>>>> features/experimental
